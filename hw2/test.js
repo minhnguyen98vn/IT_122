65e663bb74869eb4e85d40ad89077476a64f0a9f
minhnguyen98vn <63829855+minhnguyen98vn@users.noreply.github.com>
2022-05-23 19:04:42 -0700

    Update test.js

--- hw2/test.js
+++ hw2/test.js
@@ -1,14 +1,10 @@
 
 let letters = ['a','b','c','e','d'];
 console.log(letters);
-
-
 letters.sort((c1,c2) => {
     return c1 > c2
 });
 console.log(letters);
-
-
 filtered_letters = letters.filter((c) => {
     return c != 'b';
 });
